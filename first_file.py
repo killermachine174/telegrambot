469ca04284d817883de2ff6e76981f76b0af3ceb
killermachine174 <40814682+killermachine174@users.noreply.github.com>
2019-09-18 00:31:23 +0545

    first commit

--- first_file.py
+++ first_file.py
@@ -12,46 +12,31 @@ server = Flask(__name__)
 @bot.message_handler(commands=['start'])  # welcome message handler
 def send_welcome(message):
     bot.reply_to(message, 'welcome')
-@bot.message_handler(commands=['help'])  # help message handler
+
+@bot.message_handler(commands=['sabin'])  # welcome message handler
 def send_welcome(message):
-    bot.reply_to(message, 'ALPHA = FEATURES MAY NOT WORK')
-
-
-@bot.message_handler(func=lambda msg: msg.text is not None and '@' in msg.text)
-# lambda function finds messages with the '@' sign in them
-# in case msg.text doesn't exist, the handler doesn't process it
-def at_converter(message):
-    texts = message.text.split()
-    at_text = findat(texts)
-    if at_text == '@':  # in case it's just the '@', skip
-        pass
-    else:
-        insta_link = "https://instagram.com/{}".format(at_text[1:])
-        bot.reply_to(message, insta_link)
-
-
+    bot.reply_to(message, 'Sabin Loves Kripa')
 
 @bot.message_handler(commands=['help'])  # help message handler
 def send_welcome(message):
     bot.reply_to(message, 'ALPHA = FEATURES MAY NOT WORK')
 
-URL = requests.get('https://www.sharesansar.com/datewise-indices')
-souped = BS(URL.content, 'html.parser')
-table = souped.find('tbody') #works good till here
-# ######################
-nepse_trfinder = table.find('tr')
-#print(nepse_trfinder)
-nepse_trfinder1 = nepse_trfinder.find_all('td')
-single_row_data =[td.text.strip() for td in nepse_trfinder1]
-# print(single_row_data)
-final_data= pd.DataFrame([single_row_data],columns=['Index','Point','Number change','percentage'])
+@bot.message_handler(func= lambda msg: msg is not None)
+def send_welcome(message):
+    bot.reply_to(message,'Thank you for using this program')
 
-# @bot.message_handler(func= lambda msg: msg is not None)
-# def send_welcome(message):
-#     bot.reply_to(message,'Thank you for using this program')
-#
 @bot.message_handler(commands=['nepse'])  # help message handler
 def send_welcome(message):
+    URL = requests.get('https://www.sharesansar.com/datewise-indices')
+    souped = BS(URL.content, 'html.parser')
+    table = souped.find('tbody')  # works good till here
+    # ######################
+    nepse_trfinder = table.find('tr')
+    # print(nepse_trfinder)
+    nepse_trfinder1 = nepse_trfinder.find_all('td')
+    single_row_data = [td.text.strip() for td in nepse_trfinder1]
+    # print(single_row_data)
+    final_data = pd.DataFrame([single_row_data], columns=['Index', 'Point', 'Number change', 'percentage'])
     bot.reply_to(message, final_data)
 
 @server.route('/' + bot_token, methods=['POST'])
@@ -59,13 +44,11 @@ def getMessage():
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
     return "!", 200
 
-
 @server.route("/")
 def webhook():
     bot.remove_webhook()
     bot.set_webhook(url='https://pure-sands-38023.herokuapp.com/' + bot_token)
     return "!", 200
-
 
 if __name__ == "__main__":
     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
